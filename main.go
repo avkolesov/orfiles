4a4d0f38182c61022ceee7ce4eed8c8018a6fa75
Aleksandr Kolesov <36958915+avkolesov@users.noreply.github.com>
2018-06-05 11:55:05 +0300

    Add files via upload

---
+++ main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"encoding/json"
+	"html/template"
+	"io/ioutil"
+	"log"
+	"net/http"
+
+	"orolia/orfiles"
+)
+
+type page struct {
+	Msg  string
+	Text string
+}
+type conf struct {
+	Port     string `json:"port"`
+	Path     string `json:"path"`
+	FileType string `json:"filetype"`
+}
+
+var filesConf *orfiles.StFilesCnfg
+var fileExec *orfiles.StFile
+var config conf
+
+func _checkErr(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	filesConf.CheckFiles()
+	t, _ := template.ParseFiles("index.html")
+	inNames := filesConf.Names[0]
+	for i := 1; i <= len(filesConf.Names)-1; i++ {
+		inNames = inNames + "," + filesConf.Names[i]
+	}
+	t.Execute(w, &page{Text: inNames})
+}
+func deleteHandler(w http.ResponseWriter, r *http.Request) {
+	delfname := r.FormValue("delf")
+	filesConf.DelFile(delfname)
+	http.Redirect(w, r, "/", 302)
+}
+func uploadHandler(w http.ResponseWriter, r *http.Request) {
+	file, handler, err := r.FormFile("New file")
+	_checkErr(err)
+	defer file.Close()
+	filesConf.AddFile(file, handler.Filename)
+	http.Redirect(w, r, "/", 302)
+}
+func execHandler(w http.ResponseWriter, r *http.Request) {
+	t, _ := template.ParseFiles("index.html")
+	execfname := r.FormValue("execf")
+	fileExec.FileInit(execfname)
+	values := fileExec.GetFileValue(filesConf.Path)
+	filesConf.CheckFiles()
+	inNames := filesConf.Names[0]
+	for i := 1; i <= len(filesConf.Names)-1; i++ {
+		inNames = inNames + "," + filesConf.Names[i]
+	}
+	t.Execute(w, &page{Msg: values, Text: inNames})
+}
+
+func main() {
+	filesConf = new(orfiles.StFilesCnfg)
+	fileExec = new(orfiles.StFile)
+	byt, err := ioutil.ReadFile("./config.json")
+	_checkErr(err)
+	err = json.Unmarshal(byt, &config)
+	_checkErr(err)
+	filesConf.Path = config.Path
+	filesConf.Type = config.FileType
+
+	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/exec", execHandler)
+	http.HandleFunc("/upload", uploadHandler)
+	http.HandleFunc("/delete", deleteHandler)
+	http.ListenAndServe(config.Port, nil)
+}
---
+++ orfiles/orfiles.go
@@ -0,0 +1,89 @@
+package orfiles
+
+import (
+	"bufio"
+	"io"
+	"io/ioutil"
+	"log"
+	"mime/multipart"
+	"os"
+	"strconv"
+	"strings"
+)
+
+// StFilesCnfg - config orolia files
+type StFilesCnfg struct {
+	Path  string
+	Type  string
+	Names []string
+}
+
+// StFile - orolia file data
+type StFile struct {
+	Name   string
+	Values [][]string
+}
+
+func _checkErr(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+// CheckFiles - check all orolia files in derictory and get names
+func (fconf *StFilesCnfg) CheckFiles() {
+	files, err := ioutil.ReadDir(fconf.Path)
+	_checkErr(err)
+	fconf.Names = []string{}
+	for _, f := range files {
+		name := f.Name()
+		if strings.HasSuffix(name, fconf.Type) {
+			fconf.Names = append(fconf.Names, name)
+		}
+	}
+}
+
+// AddFile - add file to derictory
+func (fconf *StFilesCnfg) AddFile(file multipart.File, addName string) {
+	if addName != "" {
+		f, err := os.OpenFile(fconf.Path+"/"+addName, os.O_WRONLY|os.O_CREATE, 0666)
+		_checkErr(err)
+		defer f.Close()
+		io.Copy(f, file)
+	}
+}
+
+// DelFile - delete file from derictory
+func (fconf *StFilesCnfg) DelFile(delName string) {
+	os.Remove(fconf.Path + "/" + delName)
+}
+
+// GetFileValue - get values form orolia file
+func (f *StFile) GetFileValue(path string) string {
+	f.Values = [][]string{}
+	content, err := os.Open(path + "/" + f.Name)
+	_checkErr(err)
+	defer content.Close()
+	scanner := bufio.NewScanner(content)
+	res := `[["X","Y"],`
+	for scanner.Scan() {
+		if !strings.HasPrefix(scanner.Text(), "#") {
+			text := scanner.Text()
+			text = strings.Replace(text, "\t", "", -1)
+			if text != "" {
+				spl := strings.Split(text, " ")
+				x, _ := strconv.ParseFloat(spl[0], 64)
+				y, _ := strconv.ParseFloat(spl[1], 64)
+				res = res + "[" + strconv.FormatFloat(x, 'f', -1, 64) + "," + strconv.FormatFloat(y, 'f', -1, 64) + "],"
+				f.Values = append(f.Values, strings.Split(text, " "))
+			}
+		}
+	}
+	res = res[0:len(res)-1] + "]"
+	return res
+}
+
+// FileInit - initialization file struct
+func (f *StFile) FileInit(name string) {
+	f.Name = name
+}
